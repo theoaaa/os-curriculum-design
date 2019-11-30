2c0d123da22100ae7209f72373f6cbc15d27f854
lyyyy <1304047243@qq.com>
2019-11-30 14:27:04 +0800

    11.30

---
+++ src/view/Client/MemoryPartClient.java
@@ -0,0 +1,84 @@
+package view.Client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MemoryPartClient extends Application {
+    private static Stage primaryStage;           // 窗口 Stage 对象
+
+    private static Boolean isExisted = false;    // 窗口是否已存在
+
+    private Boolean isMinimized = false;  // 窗口是否最小化
+
+    public static  void main(String[] args) {
+          launch(args);
+      }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MemoryPart/MemoryManagementWindow.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("MemoryManagementBlock");
+        primaryStage.setScene(new Scene(root,850,500));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        /*// 设置窗口始终位于顶部
+        this.primaryStage.setAlwaysOnTop(true);
+        isExisted = true;
+        // 监听窗口关闭事件
+        this.primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                isExisted = false;
+            }
+        });
+        // 绑定 isMinimized 值
+        bindIsMinimized(scene);*/
+    }
+
+    /**
+     * 绑定 isMinimized 值与当前窗口最小化状态
+     * @param scene
+     */
+    /*private void bindIsMinimized(Scene scene) {
+        // 监听窗口 最小化/最大化 事件；oldValue 和 newValue 均表示是否最大化
+        scene.getWindow().focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                isMinimized =  !newValue;
+            }
+        });
+    }*/
+
+    /**
+     * 显示窗口
+     */
+    public void show() {
+        if (isExisted) { // 窗口是否已存在
+            // 当窗口最小化时显示窗口
+            if (isMinimized) {
+                this.primaryStage.setIconified(false);
+            }
+        } else {
+            try {
+                start(new Stage());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 单例模式
+     */
+    public static Stage getInstance(){
+        if(isExisted == false && primaryStage == null){
+            //
+        }
+        return primaryStage;
+    }
+}
